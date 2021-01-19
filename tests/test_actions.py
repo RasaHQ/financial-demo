04e4fb31531f74189a304559568b173531432fb2
Arjaan Buijk <arjaan.buijk@gmail.com>
2021-01-19 15:08:25 -0500

    Fix expected_events of test

--- tests/test_actions.py
+++ tests/test_actions.py
@@ -20,28 +20,30 @@ async def test_run_action_session_start(dispatcher, domain):
     ]
     assert events == expected_events
 
+
 async def test_action_pay_cc_not_confirmed(dispatcher, domain):
     tracker = PAY_CC_NOT_CONFIRMED
     action = actions.ActionPayCC()
     events = await action.run(dispatcher, tracker, domain)
     expected_events = [
         SlotSet("AA_CONTINUE_FORM", None),
-        SlotSet("zz_confirm_form", None), 
-        SlotSet("credit_card", None), 
-        SlotSet("account_type", None), 
-        SlotSet("amount-of-money", None), 
-        SlotSet("time", None), 
-        SlotSet("time_formatted", None), 
-        SlotSet("start_time", None), 
-        SlotSet("end_time", None), 
-        SlotSet("start_time_formatted", None), 
-        SlotSet("end_time_formatted", None), 
-        SlotSet("grain", None), 
-        SlotSet("number", None)
+        SlotSet("zz_confirm_form", None),
+        SlotSet("credit_card", None),
+        SlotSet("account_type", None),
+        SlotSet("amount-of-money", None),
+        SlotSet("time", None),
+        SlotSet("time_formatted", None),
+        SlotSet("start_time", None),
+        SlotSet("end_time", None),
+        SlotSet("start_time_formatted", None),
+        SlotSet("end_time_formatted", None),
+        SlotSet("grain", None),
+        SlotSet("number", None),
     ]
     expected_template = "utter_cc_pay_cancelled"
     assert events == expected_events
-    assert dispatcher.messages[0]['template'] == expected_template
+    assert dispatcher.messages[0]["template"] == expected_template
+
 
 async def test_action_pay_cc_confirmed(dispatcher, domain):
     tracker = EMPTY_TRACKER
@@ -52,19 +54,20 @@ async def test_action_pay_cc_confirmed(dispatcher, domain):
     events = await action.run(dispatcher, tracker, domain)
     expected_events = [
         SlotSet("AA_CONTINUE_FORM", None),
-        SlotSet("zz_confirm_form", None), 
-        SlotSet("credit_card", None), 
-        SlotSet("account_type", None), 
-        SlotSet("amount-of-money", None), 
-        SlotSet("time", None), 
-        SlotSet("time_formatted", None), 
-        SlotSet("start_time", None), 
-        SlotSet("end_time", None), 
-        SlotSet("start_time_formatted", None), 
-        SlotSet("end_time_formatted", None), 
-        SlotSet("grain", None), 
-        SlotSet("number", None)
+        SlotSet("zz_confirm_form", None),
+        SlotSet("credit_card", None),
+        SlotSet("account_type", None),
+        SlotSet("amount-of-money", None),
+        SlotSet("time", None),
+        SlotSet("time_formatted", None),
+        SlotSet("start_time", None),
+        SlotSet("end_time", None),
+        SlotSet("start_time_formatted", None),
+        SlotSet("end_time_formatted", None),
+        SlotSet("grain", None),
+        SlotSet("number", None),
+        SlotSet("amount_transferred", value=602.65),
     ]
     expected_template = "utter_cc_pay_scheduled"
     assert events == expected_events
-    assert dispatcher.messages[0]['template'] == expected_template
+    assert dispatcher.messages[0]["template"] == expected_template
