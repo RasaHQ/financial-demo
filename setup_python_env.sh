99c3498577f9b090dfc889aed78941a30909e361
hsm207 <hsm207@users.noreply.github.com>
2021-04-09 15:12:15 +0000

    Update python setup script

--- setup_python_env.sh
+++ setup_python_env.sh
@@ -7,3 +7,8 @@ source ./.venv/bin/activate
 # need at least pip 19.0 to install poetry project
 # see: https://github.com/python-poetry/poetry/issues/321#issuecomment-458205972
 python -m pip install -U pip
+
+pip install -r requirements.txt
+
+python -m spacy download en_core_web_md
+python -m spacy link en_core_web_md en
